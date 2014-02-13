5157c7ffeaeb36a3ef7419d3fddcba3f819c9e57
Roger Whitney <whitney@cs.sdsu.edu>
2014-02-13 12:59:03 -0800

    Without layout

---
+++ src/com/example/listexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.listexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends ListActivity {
+	private static final String[] items={"lorem", "ipsum", "dolor",
+		"sit", "amet", "consectetuer", "adipiscing", "elit", "morbi", "vel",
+		"ligula", "vitae", "arcu", "aliquet", "mollis",
+		"etiam", "vel", "erat", "placerat", "ante",
+		"porttitor", "sodales", "pellentesque", "augue", "purus"};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, items));
+	}
+
+
+}
