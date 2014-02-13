806e621eed537cfcb277a433f87babab748dad97
Roger Whitney <whitney@cs.sdsu.edu>
2014-02-13 13:09:12 -0800

    WithLayout - layout has issues

--- src/com/example/listexample/MainActivity.java
+++ src/com/example/listexample/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ListActivity {
+	private TextView selection;
 	private static final String[] items={"lorem", "ipsum", "dolor",
 		"sit", "amet", "consectetuer", "adipiscing", "elit", "morbi", "vel",
 		"ligula", "vitae", "arcu", "aliquet", "mollis",
@@ -16,9 +20,15 @@ public class MainActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		selection=(TextView)findViewById(R.id.selection);
 		setListAdapter(new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, items));
 	}
 
+	public void onListItemClick(ListView parent, View v, int position,
+			long id) {
+		selection.setText(items[position]);
+	}
 
 }
